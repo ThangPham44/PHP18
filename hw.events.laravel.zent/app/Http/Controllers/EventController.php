b9e0f0ae77909275e91271f19004a83aba58199e
Thắng Phạm <thangpham44>
2019-05-27 13:02:38 +0700

    hw.event

---
+++ hw.events.laravel.zent/app/Http/Controllers/EventController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+  public function index(Request $request)
+	{
+		$events = Event::all();
+        // dd($todos);
+         // return views(route('todos.index'));
+		return view('event',compact('events'));
+	}
+
+	public function create()
+	{
+		return view('add');
+	}
+	  public function store(Request $request)
+    {
+       Event::create($request->all());
+        return redirect(route('events.index'));
+    }
+
+      public function edit(Request $request,$id)
+    {
+       $event= Event::find($id);
+       return view('edit',compact('event','id'));
+        // return redirect(route('todos.index'));
+    }
+      public function update(Request $request,$id)
+    {
+        Event::find($id)->update($request->all());
+        return redirect(route('events.index'));
+    }
+
+     public function destroy($id)
+    {
+        Event::find($id)->delete();
+        return redirect(route('events.index'));
+    }
+}
